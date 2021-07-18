853417588dfcc861a357e7387b33e09c65204b31
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-07-18 15:19:47 +0100

    updated index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import GrilleBlog from '../components/molecule/GrilleBlog'
-import GrilleBlogLeft from '../components/molecule/GrilleBlogLeft'
-import CardMinSection from '../components/molecule/SectionCardMin'
-import Slider from '../components/molecule/Slider'
-import { theme } from '../theme'
+import Head from "next/head";
+import GrilleBlog from "../components/molecule/GrilleBlog";
+import GrilleBlogLeft from "../components/molecule/GrilleBlogLeft";
+import CardMinSection from "../components/molecule/SectionCardMin";
+import Slider from "../components/molecule/Slider";
+import { theme } from "../theme";
+import SectionApropos from "../components/SectionApropos";
 
 export default function Home() {
   return (
@@ -12,27 +13,30 @@ export default function Home() {
         <title>Métrologie</title>
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
-      <main className="main" >
-       <Slider />
-        <section className="kingSection sectionHeader ml-0 mr-0 mt-60 mb-60 pt-60 pb-60"  style={{
-       background: theme.colors.kingGreen
-       }}>
-         <div className="container">
-           <GrilleBlog letGo={{}} />
-         </div>
-       </section>
-       <section>
-         <div className="container">
-           <GrilleBlogLeft letGo={{}} />
-         </div>
-       </section>
-       <section>
-         <div className="container">
-           <CardMinSection letGo={{}} />
-         </div>
-       </section>
+      <main className="main">
+        <Slider />
+        <SectionApropos />
+        <section
+          className=""
+          style={{
+            background: theme.colors.kingGreen,
+          }}
+        >
+          <div className="container">
+            <GrilleBlog letGo={{}} />
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <GrilleBlogLeft letGo={{}} />
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <CardMinSection letGo={{}} />
+          </div>
+        </section>
       </main>
-
     </div>
-  )
+  );
 }
