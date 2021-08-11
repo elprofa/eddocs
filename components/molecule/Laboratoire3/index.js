98ccb12222833d2c2943c0fe10d94f96509d0a08
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-08-11 12:55:34 +0100

    Lab3

--- components/molecule/Laboratoire3/index.js
+++ components/molecule/Laboratoire3/index.js
@@ -20,17 +20,47 @@ const Laboratoire3 = (props) => {
                                     d'un laboratoire de haute performance qui assure l'étalonnage de vos
                                     instruments de mesure en dimensionnel sous accréditation:"  
                                     MyclassName="paragraphe" />
+                </div>
 
+                </div>
+            <div className="col-lg-12">
+                <div className="rectangle">
+                    <div>
+                        <img src="/img/7.png" />
+                    </div>
+                    <div>
+                        <img src="/img/8.png" />
+                    </div>
+                    <div>
+                        <img src="/img/Groupe -9.png" />
+                    </div>
+                    <div>
+                        <img src="/img/8.png" />
+                    </div>
+                    <div>
+                        <img src="/img/ilac-ment-1.png" />
+                    </div>
+                    <div>
+                        <img src="/img/8.png" />
+                    </div>
+                    <div>
+                        <img src="/img/Groupe -8.png" />
+                    </div>
+                    <div>
+                        <img src="/img/8.png" />
+                    </div>
+                </div>
 
-                    <Texte texte="ou sous étalonnage raccordé au système international d'unité SI, ceci au
+                <div className="col-lg-6">                
+                <Texte texte="ou sous étalonnage raccordé au système international d'unité SI, ceci au
                                     moyen d'étalons et machines de mesures de références étalonnés par des
                                     laboratoires accrédités par le COFRAC."  
                                     MyclassName="paragraphe" />
+                </div>
 
                 <Button texte="Demander un devis >" className="my-2 my-md-3"/>
                 </div>
             </div>
-        </div>
 
         <div className="row">
             <div className="col-lg-4">
@@ -40,15 +70,29 @@ const Laboratoire3 = (props) => {
         
         <div className="row py-2 py-md-0">
            
-            <div className="col-lg-6">
-                <img src="/img/Rectangle69.png" width="100%" />
-                <Title partOne="Presentation Mécanique" 
-                    PartTwo="" number={1}  MyclassName="py-2 py-md-3 title" />
-            </div> 
-            <div className="col-lg-6">
-                <img src="/img/Rectangle69.png" width="100%" />
-
-                <Texte texte="Le lorem ipsum est"  MyclassName="paragraphe" />
+            <div className="col-lg-12">
+            <div className="rectangle">
+                    <div>
+                        <img src="/img/Groupe 210.png" />
+                        <span>Présentation Mécanique</span>
+                    </div>
+                    <div>
+                        <img src="/img/rect-e4.png" />
+                        <span>Presentation Interferometrique</span>
+                    </div>
+                    <div>
+                        <img src="/img/Rectangle 119.png" />
+                        <span>Presentation Angulaire</span>
+                    </div>
+                    <div>
+                        <img src="/img/Rectangle 120.png" />
+                        <span>Presentation 2D</span>
+                    </div>
+                    <div>
+                        <img src="/img/Groupe 211.png" />
+                        <span>Presentation 3D</span>
+                    </div>
+                </div>
             </div> 
         </div>
     </LaboratoireStc3>
