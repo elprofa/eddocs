6aace24c132e5210fc977134395c3f330e423fff
elprofa <76491497+elprofa@users.noreply.github.com>
2021-08-14 06:29:54 +0100

    update140820211104

--- components/molecule/SectionNosLaboratoireEssais/index.js
+++ components/molecule/SectionNosLaboratoireEssais/index.js
@@ -2,36 +2,36 @@
 import Texte from "../../shared/Texte";
 import CardMin from "../CardMin";
 import SectionNosLaboratoireEssaisStc from "./SectionNosLaboratoireEssais.stc"
-
+import {Row} from 'reactstrap'
 const SectionNosLaboratoireEssais = () => {
     const laboratoires=[
         {
-            src:"/img/enc_clim.png",
+            src:"/img/icon/14.png",
             texte:<>Enceinte climatique <br/></>,
         },
         {
-            src:"/img/autoclave.png",
+            src:"/img/icon/15.png",
             texte:"Autoclaves",
         },
         {
-            src:"/img/air-comp.png",
+            src:"/img/icon/16.png",
             texte:"Qualification air compagnie",
         }
         ,
         {
-            src:"/img/envi-propres.png",
+            src:"/img/icon/17.png",
             texte:"Evironnement",
         }
     ]
   return (
     <SectionNosLaboratoireEssaisStc className="py-2 py-md-3">
-        <div className="col-lg-12">
-            <Texte texte="Essais" MyclassName="paragraphe px-2 px-md-3" />
-            <div className="divCard">
+        <Texte texte="Nos domaine de Essais" MyclassName="paragraphe px-2 px-md-3" />
+        <div className="divcard">
+             <Row>
                 {
                     laboratoires.map((labo,index)=><CardMin key={index+"labo"} letGo={labo} />)
                 }
-            </div>
+            </Row>
         </div>
     </SectionNosLaboratoireEssaisStc>
   );
