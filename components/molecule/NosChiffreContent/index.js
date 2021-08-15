dee0e9e9af046bd8af050bce848902a457fa9385
elprofa <76491497+elprofa@users.noreply.github.com>
2021-08-15 04:52:49 +0100

    animation deu chiffre 20 ans d'experience

--- components/molecule/NosChiffreContent/index.js
+++ components/molecule/NosChiffreContent/index.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import { useEffect } from "react";
 import { theme } from '../../../theme';
 import Title from '../../shared/Title';
 import Trait from '../../shared/Trait';
 import Texte from '../../shared/Texte';
 import NoschiffreContentStc from './NosChiffreContent.stc';
-
+import {gsap} from 'gsap'
+import { ScrollTrigger,GSDevTools } from "gsap/dist/ScrollTrigger";
 
 const NoschiffreContent = (props) => {
-
     const letGo=props.letGo;
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        
+        gsap.to(".wedo",{
+                scrollTrigger:{
+                   trigger: ".wedo",
+                   markers:true,
+                   start:"top bottom",
+                   toggleActions:'restart none restart none'
+                },
+                innerText:20,
+                snap: {
+                    innerText:1
+                  } ,
+                duration:2
+            }
+        )
+      }, []); 
+   
 
     return (
         <NoschiffreContentStc MonStyle={letGo.style} className="py-2 py-md-5">
@@ -17,9 +38,9 @@ const NoschiffreContent = (props) => {
             <div className="row py-2 py-md-3">
                 <div className="col-md-12">
                     <ul className="ulchiffres px-0 px-md-0 py-3 my-0 px-md-0">
-                        <li>
+                        <li className="">
                             
-                            <Texte MyclassName="h2 py-2 py-md-0" texte={<><span className="plus">+</span> 20
+                            <Texte MyclassName="h2 py-2 py-md-0" texte={<><span className="plus">+</span> <span className="wedo">0</span>
                             <span className="ans">ans</span>
                             <Texte MyclassName="label mb-0 mb-md-3" texte="Experiences" />
                             </>} />
