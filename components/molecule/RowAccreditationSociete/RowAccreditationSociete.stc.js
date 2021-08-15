f7fb0e12e905515fec2e5dc4262c9db662f00eb0
elprofa <76491497+elprofa@users.noreply.github.com>
2021-08-15 22:40:50 +0100

    update petite responsivité

---
+++ components/molecule/RowAccreditationSociete/RowAccreditationSociete.stc.js
@@ -0,0 +1,9 @@
+import Styled from 'styled-components';
+import { Row } from "reactstrap";
+const RowAccreditationSocieteStc=Styled(Row)`
+    @media screen and (max-width: 476px)
+    {
+        text-align:center;
+    }
+`;
+export default RowAccreditationSocieteStc;
--- components/molecule/RowAccreditationSociete/index.js
+++ components/molecule/RowAccreditationSociete/index.js
@@ -1,27 +1,27 @@
 import {Row,Col} from 'reactstrap'
 import PCard from "../PresentationCard";
-
+import RowAccreditationSocieteStc from './RowAccreditationSociete.stc'
 const RowAccreditationSociete=(props)=>{
     if(props.type==1)
     {
         return (
-            <Row className="my-2 my-md-3">
+            <RowAccreditationSocieteStc className="my-2 my-md-3">
                 <Col lg={6} md={6} className="align-self-center">
                     <h6 className="fw-bold">{props.texte}</h6>
                 </Col>
                 <PCard url1={props.url1} url2={props.url2} type={1}  />
-            </Row>
+            </RowAccreditationSocieteStc>
         )
     }
     else
     {
      return (
-            <Row className="my-2 my-md-3">
+            <RowAccreditationSocieteStc className="my-2 my-md-3">
                 <Col lg={12} md={12} className="align-self-center">
                     <h6 className="fw-bold">{props.texte}</h6>
                 </Col>
                 
-            </Row>
+            </RowAccreditationSocieteStc>
         )
     }
 }
