81671ccc161eb050a2d891bbd621ac3b7ce50410
elprofa <wedoprofan14@gmail.com>
2022-02-10 03:50:02 +0100

    Last integration Labo14

--- components/molecule/Laboratoire14/index.js
+++ components/molecule/Laboratoire14/index.js
@@ -4,7 +4,7 @@ import { theme } from '../../../theme';
 import Title from '../../shared/Title';
 import Texte from '../../shared/Texte';
 import Button from '../../shared/Button';
-import LaboratoireStc1 from '../Laboratoire2/Laboratoire.stc';
+import LaboratoireStc14 from './laboratoire.stc';
 import TitreParagrapheLabo from '../TitreParagrapheLabo';
 import {Row,Col} from 'reactstrap'
 import PCard from "../PresentationCard";
@@ -13,7 +13,7 @@ import HeaderLabo from '../HeaderLabo';
 const Laboratoire14 = (props) => {
   
   return (
-    <LaboratoireStc1 >
+    <LaboratoireStc14 >
 
         <HeaderLabo img="/img/air/F4.png" title={{
             partOne:"Qualification Des ",
@@ -37,23 +37,62 @@ const Laboratoire14 = (props) => {
                         suivant les tests suivants :" />
 
                 <div className="row my-2 my-md-4">
-                    <div className="col-lg-6 col-md-8">
-                        <ul> 
-                            <li className="fw-bold">Comptage de particule</li>
-                            <li className="fw-bold">Mesure de vitesse d'air</li>
-                            <li className="fw-bold">Mesure de pression differentielle</li>
-                            <li className="fw-bold">Test d'intégrité des filtres</li>
-                            <li className="fw-bold">Mesure de température </li>
-                            <li className="fw-bold">Mésure d'humidité relative</li>
-                            <li className="fw-bold">Mésure de debit d'air</li>
-                            <li className="fw-bold">Essai de fuite de confinement </li>
-                        </ul>
-                        <Button texte="Demandez un devis >" className="demande_devis " />
+                    <div className="col-lg-4 col-md-6">
+                        <div>
+                            <h5>
+                                SALLES PROPRES & ENVIRONNEMENTS MAITRISÉS ET APPARENTÉS :
+                            </h5>
+                            <ul > 
+                                <li className="">Comptage de particule</li>
+                                <li className="">Mesure de vitesse d'air</li>
+                                <li className="">Mesure de pression differentielle</li>
+                                <li className="">Test d'intégrité des filtres</li>
+                                <li className="">Mesure de température </li>
+                                <li className="">Mésure d'humidité relative</li>
+                                <li className="">Mésure de debit d'air</li>
+                                <li className="">Essai de fuite de confinement </li>
+                            </ul>
+                        </div>
+                        
                     </div>
-                    <div className="col-md-6">
-                        <img src="/img/environnement/110.png" width="100%"/>
+                    <div className="col-lg-4 col-md-6">
+                        <div>
+                            <h5>
+                                ETABLISSEMENT DE SANTÉ – ZONES ENVIRENEMENT MAITRISÉS : 
+                            </h5>
+                            <ul > 
+                                <li className="">Comptage de particule</li>
+                                <li className="">Mesure de vitesse d'air</li>
+                                <li className="">Mesure de pression differentielle</li>
+                                <li className="">Test d'intégrité des filtres</li>
+                                <li className="">Mesure de température </li>
+                                <li className="">Mésure d'humidité relative</li>
+                                <li className="">Mésure de debit d'air</li>
+                                <li className="">Essai de fuite de confinement </li>
+                            </ul>
+                        </div>
+                        
                     </div>
+                    <div className="col-lg-4 col-md-6">
+                        <div>
+                            <h5>
+                                POSTE DE SECURITÉ MICROBIOLOGIQUE PSM type 2 :
+                            </h5>
+                            <ul >
+                                <li className="">Comptage de particule</li>
+                                <li className="">Vitesse descendante</li>
+                                <li className="">Mesure de pression differentielle</li>
+                                <li className="">Test d'intégrité des filtres</li>
+                                <li className="">Mesure de température </li>
+                                <li className="">Mésure d'humidité relative</li>
+                                <li className="">Mésure de debit volumique</li>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                    
                 </div>
+                <Button texte="Demandez un devis >" className="demande_devis " />
             </div>
         </div>
             
@@ -62,7 +101,7 @@ const Laboratoire14 = (props) => {
             <PCard type="4" url1="/img/environnement/302.png" title=""  />
             <PCard type="4" url1="/img/environnement/303.png" title=""  />
         </div>
-</LaboratoireStc1>
+</LaboratoireStc14>
   );
 }
 
